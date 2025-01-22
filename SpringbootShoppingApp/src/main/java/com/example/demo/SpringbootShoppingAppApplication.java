88b01ffd092dd97c78b3c49d87853f95c2ac5b0e
amber gautam <gautamamber3@gmail.com>
2025-01-22 11:07:07 +0530

    First commit

---
+++ SpringbootShoppingApp/src/main/java/com/example/demo/SpringbootShoppingAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShoppingAppApplication.class, args);
+		
+	}
+
+}
